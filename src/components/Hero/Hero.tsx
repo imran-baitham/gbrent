c0872e00c8825098c20de15b70949e5868612fc8
imran-baitham <imranbaitham001@gmail.com>
2023-01-23 13:38:50 +0500

    update hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -15,13 +15,14 @@ import 'swiper/css/pagination'
 // import required modules
 import { Autoplay, Pagination } from 'swiper'
 import { useTheme } from 'next-themes'
+import { Button } from '../Button/Button'
 
 let data = [
-  { id: 1, name: 'Html5', image: html },
-  { id: 2, name: 'Css3', image: css },
-  { id: 3, name: 'JavaScript', image: javascript },
-  { id: 11, name: 'Git', image: git },
-  { id: 12, name: 'C++', image: cpp },
+  { id: 1, name: 'Seasons by Porcelite', image: html },
+  { id: 2, name: 'Free Survey', image: css },
+  { id: 3, name: 'Zanolli Pizza Ovens', image: javascript },
+  { id: 11, name: 'Seasons by Free Survey', image: git },
+  { id: 12, name: 'Extensive Glassware Range for any occasion ', image: cpp },
 ]
 
 export function Hero() {
@@ -54,7 +55,7 @@ export function Hero() {
           return (
             <SwiperSlide key={index} className="w-full dots">
               <div
-                className="h-[300px] sm:h-[500px] lg:h-[500px] xl:h-[700px]"
+                className="h-[330px] sm:h-[500px] lg:h-[500px] xl:h-[700px]"
                 style={{
                   backgroundImage: `url(${item.image.src})`,
                   backgroundSize: 'cover',
@@ -65,10 +66,18 @@ export function Hero() {
                     theme === 'dark' ? 'rgba(0,0,0,0.7)' : 'rgba(0,0,0,0.4)', //'rgba(255,25,25,0.9)',
                 }}
               >
-                <div className="w-full h-full flex items-center justify-center">
-                  <h1 className="text-xl md:text-4xl text-white font-bold">
-                    E2E - HotelWare
-                  </h1>
+                <div className="w-full h-full flex items-center container_main">
+                  <div className="md:w-3/4">
+                    <h1 className="text-md md:text-2xl text-white font-bold pb-2 md:pb-4">
+                      E2E - HotelWare
+                    </h1>
+                    <h1 className="text-4xl md:text-[80px] text-white font-samibold md:leading-[85px] pb-4 font-[Merriweather]">
+                      {item.name}
+                    </h1>
+                    <Button primary xl className="px-5 md:px-10">
+                      Explore More
+                    </Button>
+                  </div>
                 </div>
               </div>
             </SwiperSlide>
