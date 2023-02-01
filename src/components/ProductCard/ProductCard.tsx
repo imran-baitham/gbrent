bb07d13e0328101724741bdd5e5b2d8a59701792
imran-baitham <imranbaitham001@gmail.com>
2023-02-01 13:08:09 +0500

    update new tabs and more code

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -7,16 +7,9 @@ import 'swiper/css/pagination'
 
 // import required modules
 import { Pagination } from 'swiper'
-import Image from 'next/image'
-import { useState } from 'react'
-import { ReactIcon } from '../ReactIcon/ReactIcon'
 import { StaticProps } from '@/utils'
 import { useRouter } from 'next/router'
-import { Heart } from '../Counter/Counter'
-
-interface RatingProps {
-  rating: number[]
-}
+import { Card } from '../Reusable/Card/Card'
 
 export function ProductCard({ ProductData }: StaticProps) {
   const router = useRouter()
@@ -52,84 +45,18 @@ export function ProductCard({ ProductData }: StaticProps) {
               key={index}
               onClick={() => router.push(`${item.slug}`)}
             >
-              <div className="mb-10 rounded-sm bg-gray-100 dark:bg-zinc-800 mt-3 cursor-pointer">
-                <div className="h-[120px] md:h-[160px] w-full relative">
-                  <Image
-                    layout="fill"
-                    src={item.productIMG}
-                    alt={item.productname}
-                    className={
-                      'border-[1px] border-gray-100 dark:border-zinc-800 rounded-t-sm'
-                    }
-                  />
-                  <button className="font-bold absolute right-1 bottom-1 bg-black text-white overflow-hidden z-10 px-1 text-[12px]">
-                    Rs {item.productprice}
-                  </button>
-                </div>
-                <div className="p-3 flex justify-between items-center">
-                  <div className="w-full">
-                    <div className="flex justify-between">
-                      <span className="text-black dark:text-white text-[10px]">
-                        {item.category}
-                      </span>
-                      <span className="text-black dark:text-white text-[10px]">
-                        {item.availability}
-                      </span>
-                    </div>
-                    <h1 className="rounded-sm uppercase text-[15px] md:text-[16px] font-bold py-1">
-                      {item.productname}
-                    </h1>
-                    <div className="py-1 flex items-center justify-between">
-                      <div className="flex items-center w-full">
-                        <ReactIcon
-                          icon="HiLocationMarker"
-                          className="w-3 h-3"
-                        />
-                        <h3 className="pl-1 text-[13px]">{item.location}</h3>
-                      </div>
-                      <div className="flex justify-end items-center w-full">
-                        <button onClick={(e) => e.stopPropagation()}>
-                          <Heart />
-                        </button>
-                      </div>
-                    </div>
-                    <h5>
-                      <Review rating={[1, 2, 3, 4, 5]} />
-                    </h5>
-                  </div>
-                </div>
-              </div>
+              <Card
+                productIMG={item.productIMG}
+                productname={item.productname}
+                productprice={item.productprice}
+                category={item.category}
+                availability={item.availability}
+                location={item.location}
+              />
             </SwiperSlide>
           )
         })}
       </Swiper>
-    </div>
-  )
-}
-
-function Review({ rating }: RatingProps) {
-  const [reviews, setReviews] = useState<number>(5)
-
-  return (
-    <div className="flex items-center">
-      {rating.map((rating, index) => {
-        return (
-          <svg
-            key={index}
-            width="13"
-            height="17"
-            viewBox="0 0 18 17"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M9 0L11.0206 6.21885H17.5595L12.2694 10.0623L14.2901 16.2812L9 12.4377L3.70993 16.2812L5.73056 10.0623L0.440492 6.21885H6.97937L9 0Z"
-              fill="#FFA800"
-            ></path>
-          </svg>
-        )
-      })}
-      <span className="text-[11px] pl-2 pt-1">{reviews} Reviews</span>
     </div>
   )
 }
